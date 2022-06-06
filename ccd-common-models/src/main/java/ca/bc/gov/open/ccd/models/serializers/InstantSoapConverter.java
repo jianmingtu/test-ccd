dfd225cfe0b30e0870ac02f0ab1cdc7c93247e2e
Sylar Z. Zhang <Sylar.Zhang@gov.bc.ca>
2022-06-06 15:49:07 -0700

    time

--- ccd-common-models/src/main/java/ca/bc/gov/open/ccd/models/serializers/InstantSoapConverter.java
+++ ccd-common-models/src/main/java/ca/bc/gov/open/ccd/models/serializers/InstantSoapConverter.java
@@ -41,17 +41,19 @@ public final class InstantSoapConverter {
                     d = sdf.parse(value);
                 } catch (ParseException ex2) {
                     try {
-                        var sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+                        var sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
                         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+                        log.warn("time: " + value);
+                        // yyyy-MM-dd
+                        if (value.length() == 10) {
+                            value += " 12:00:00";
+                        }
                         d = sdf.parse(value);
                     } catch (ParseException ex3) {
                         try {
-                            var sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+                            var sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
                             sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-                            // yyyy-MM-dd
-                            if (value.length() == 10) {
-                                value += " 12:00:00";
-                            }
+                            log.warn("time: " + value);
                             d = sdf.parse(value);
                         } catch (ParseException ex4) {
                             return Instant.parse(value + "Z");
