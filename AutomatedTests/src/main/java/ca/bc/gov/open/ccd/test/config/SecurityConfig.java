f41b96afb9d451b0d0c3da8f5161a33907b6bfa6
jianming tu <tu_jm@yahoo.com>
2023-09-18 13:11:34 -0700

    fix Swagger failure after upgrade java 17

--- AutomatedTests/src/main/java/ca/bc/gov/open/ccd/test/config/SecurityConfig.java
+++ AutomatedTests/src/main/java/ca/bc/gov/open/ccd/test/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -25,32 +26,6 @@ public class SecurityConfig {
 
     @Autowired private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                authorizeRequests -> {
-                    authorizeRequests.anyRequest().authenticated();
-                });
-
-        http.sessionManagement(
-                httpSecuritySessionManagementConfigurer -> {
-                    httpSecuritySessionManagementConfigurer.sessionCreationPolicy(
-                            SessionCreationPolicy.STATELESS);
-                });
-
-        http.httpBasic(
-                httpSecurityHttpBasicConfigurer -> {
-                    httpSecurityHttpBasicConfigurer.authenticationEntryPoint(
-                            authenticationEntryPoint);
-                });
-
-        http.csrf(
-                httpSecurityCsrfConfigurer -> {
-                    httpSecurityCsrfConfigurer.disable();
-                });
-
-        return http.build();
-    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
@@ -58,8 +33,19 @@ public class SecurityConfig {
                 User.builder()
                         .username(userName)
                         .password(passwordEncoder().encode(password))
+                        .roles("Admin")
                         .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/openapi/openapi.yml")).permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic();
+        return http.build();
     }
 
     @Bean
